f8370ae3a1543d92ab2311abaf1227feb95019c3
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-18 12:57:46 +0330

    added token validation to program.cs

--- ShrinkLink.Auth.Service/Program.cs
+++ ShrinkLink.Auth.Service/Program.cs
@@ -1,4 +1,12 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ShrinkLink.Auth.Service.Models.Interfaces;
+using ShrinkLink.Auth.Service.Repositories;
+using ShrinkLink.Auth.Service.Services;
+using System.Text;
+
 namespace ShrinkLink.Auth.Service
 {
     public class Program
@@ -8,6 +16,36 @@ namespace ShrinkLink.Auth.Service
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("Cn1");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'Cn1' not found.");
+            }
+            builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+
+
+
+            builder.Services.AddScoped<AuthorizationService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

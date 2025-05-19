b2db209a29e0c8eaebb7169d75d74a8eac78778b
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-19 14:35:02 +0330

    added some actions to redirect , register and login

--- ShrinkLink.WebApp/Controllers/DashboardController.cs
+++ ShrinkLink.WebApp/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ShrinkLink.WebApp.attribute;
 namespace ShrinkLink.WebApp.Controllers
 {
+    [JwtAuthorize]
     public class DashboardController : Controller
     {
         public IActionResult Index()
--- ShrinkLink.WebApp/Controllers/HomeController.cs
+++ ShrinkLink.WebApp/Controllers/HomeController.cs
@@ -1,51 +1,113 @@
 using Microsoft.AspNetCore.Mvc;
 using ShrinkLink.WebApp.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using ShrinkLink.WebApp.Models.Dtos;
 using ShrinkLink.WebApp.Services;
+using NuGet.Common;
+using ShrinkLink.WebApp.attribute;
 
 namespace ShrinkLink.WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        public readonly WebRequestHandlerApplication _application;
+        public readonly WebRequestHandler _application;
 
-        public HomeController(ILogger<HomeController> logger, WebRequestHandlerApplication application)
+        public HomeController(ILogger<HomeController> logger, WebRequestHandler application)
         {
             _logger = logger;
             _application = application;
         }
-
-
-        [HttpPost]
-        public async Task<IActionResult> PostLongUrl(ShortenerViewModel model)
-        {
-
-            Console.WriteLine(model.Request.LongLink);
-            var result = await _application.PostLinkHandlerAsync(model.Request);
-            Console.WriteLine(result.ShortUrl);
-
-
-            var test = new ShortenerViewModel
-            {
-                Request = model.Request,
-                Response = result
-            };
-
-
-            return View("Index",test);
-        }
-
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [JwtAuthorize]
+        public IActionResult RedirectToDashboard()
         {
-            return View();
+            return Redirect("/Dashboard/");
+        }
+
+        public IActionResult RedirectBaseedOnAuth()
+        {
+            var fetchedToken = Request.Cookies["jwt-token"];
+            if (!string.IsNullOrEmpty(fetchedToken))
+            {
+                return RedirectToAction("RedirectToDashboard");
+            }
+            else
+            {
+                return RedirectToAction("LoginPage");
+            }
+        }
+
+        public IActionResult RegisterPage()
+        {
+            return View("RegisterPage");
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View("LoginPage");
+        }
+
+
+        //aslan in inja nabayad bashe in bayad too dashboard bashe 
+        //[HttpPost]
+        //public async Task<IActionResult> PostLongUrl(ShortenerModel request)
+        //{
+        //    var token = Request.Cookies["jwt-token"];
+
+        //    if (token == null)
+        //    {
+        //        return RedirectToAction("Login");
+        //    }
+
+        //    var result = await _application.PostLinkHandlerAsync(request.Request, token);
+        //    var test = new ShortenerModel
+        //    {
+        //        Request = request.Request,
+        //        Response = result
+        //    };
+
+        //    return View("Index", test);
+        //}
+
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Register(RegisterModel model)
+        //{
+        //    var result = _application.PostRegisterRequest(model.TransferObject);
+
+
+        //}
+
+
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var result = await _application.PostLoginRequest(model.TransferObject);
+            Response.Cookies.Append("jwt-token", result.Token, new CookieOptions()
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = result.ExpiresAt
+            });
+
+            if (result.Token != null)
+            {
+                return RedirectToAction("RedirectToDashboard");
+            }
+
+            return BadRequest("can not login"); // this must be worked at
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

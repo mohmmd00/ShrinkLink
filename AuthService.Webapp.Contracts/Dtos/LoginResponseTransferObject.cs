2f2ce9a53eb14fb84a2981cb4f4a83e468cb8c98
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-19 13:08:49 +0330

    created a response to register operation and renamed auth response to login response

--- AuthService.Webapp.Contracts/Dtos/LoginResponseTransferObject.cs
+++ AuthService.Webapp.Contracts/Dtos/LoginResponseTransferObject.cs
@@ -1,6 +1,6 @@
 ﻿namespace AuthService.Webapp.Contracts.Dtos;
 
-public class AuthResponseTransferObject
+public class LoginResponseTransferObject
 {
     public string Token { get; set; }
     public DateTime ExpiresAt { get; set; }
---
+++ AuthService.Webapp.Contracts/Dtos/RegisterResponseTransferObject.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthService.Webapp.Contracts.Dtos
+{
+    public class RegisterResponseTransferObject
+    {
+        public string Message { get; set; }
+    }
+}

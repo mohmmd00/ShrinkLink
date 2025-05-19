c00d93ee577a16efc355da3d6ca68de63b49ac82
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-19 12:46:02 +0330

    craeted a binding model for register page

---
+++ ShrinkLink.WebApp/Models/Dtos/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Webapp.Contracts.Dtos;
+
+namespace ShrinkLink.WebApp.Models.Dtos
+{
+    public class RegisterModel
+    {
+        public RegisterTransferObject TransferObject { get; set; }
+        public RegisterResponseTransferObject ResponseTransferObject { get; set; }
+    }
+}

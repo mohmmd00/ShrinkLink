9924529fd13ed67e6e5bf1a10fdafac35fc7f190
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-19 15:59:09 +0330

    created a attribute to validate jwt

---
+++ ShrinkLink.WebApp/Attribute/JwtAuthorizeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ShrinkLink.WebApp.attribute
+{
+    public class JwtAuthorizeAttribute : Attribute , IAsyncAuthorizationFilter
+    {
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext.Request.Cookies["jwt-token"];
+            if (string.IsNullOrEmpty(token))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary()
+                {
+                    { "Controller", "Home" },
+                    { "Action", "LoginPage" }
+                });
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}

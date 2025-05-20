9e1724ed797f2022ec78b5fe862dce5b26e28b9a
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-20 17:30:43 +0330

    re worked this entity and changed some properties

--- ShrinkLink.UrlShortener.Service/Models/Entities/ProcessedUrl.cs
+++ ShrinkLink.UrlShortener.Service/Models/Entities/ProcessedUrl.cs
@@ -4,18 +4,18 @@
     {
         public Guid Id { get; }
         public string OriginalUrl { get; private set; }
-        public string ShortUrl { get; private set; }
+        public string Username { get; private set; }
         public string Code { get; private set; }
         public string CreatedAt { get; private set; } // beware of the change of this property !!!!
 
         public List<Visitor> Visitors { get; set; } //navigation prop
 
 
-        public ProcessedUrl(string originalUrl, string code, string shortUrl)
+        public ProcessedUrl(string originalUrl, string code, string username)
         {
             Id = Guid.NewGuid();
+            Username = username;
             OriginalUrl = originalUrl;
-            ShortUrl = shortUrl;
             Code = code;
             CreatedAt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         }

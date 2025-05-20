8130bcc7f0ea8c4d8f9a113ed942e27a45e1779b
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-20 17:08:09 +0330

    added a data transfer object for user basic information

---
+++ ShrinkLink.Auth.Service/Models/DataTransferObjects/UserBaseInformationTransferObject.cs
@@ -0,0 +1,8 @@
+﻿namespace ShrinkLink.Auth.Service.Models.DataTransferObjects
+{
+    public class UserBaseInformationTransferObject
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}

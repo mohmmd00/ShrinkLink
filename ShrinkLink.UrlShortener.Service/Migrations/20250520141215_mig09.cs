2c843cd337c5585f4fe1b0f18a93cc6205ce472a
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2025-05-21 10:57:57 +0330

    added some migrations to update new database that has beem created

---
+++ ShrinkLink.UrlShortener.Service/Migrations/20250520141215_mig09.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShrinkLink.UrlShortener.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig09 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortUrl",
+                table: "ShortenUrl");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "ShortenUrl",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ShortenUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortUrl",
+                table: "ShortenUrl",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ ShrinkLink.UrlShortener.Service/Migrations/20250520142954_mig10.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShrinkLink.UrlShortener.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
